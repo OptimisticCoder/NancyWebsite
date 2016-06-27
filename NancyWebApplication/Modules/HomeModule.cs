b5f7e2f126c6976f4bd33b9484044efa663ae038
OptimisticCoder <danpatching@gmail.com>
2016-06-27 20:55:56 +0100

    Added files.

---
+++ NancyWebApplication/Modules/HomeModule.cs
@@ -0,0 +1,18 @@
+﻿namespace NancyWebApplication.Modules
+{
+    using Nancy;
+    using System.Configuration;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                dynamic viewBag = new DynamicDictionary();
+                viewBag.OurWebLink = ConfigurationManager.AppSettings["OurWebLink"];
+                return View["Home", viewBag];
+            };
+        }
+    }
+}
